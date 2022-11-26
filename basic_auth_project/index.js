86847e648cce81bb1c20f5b91a9b992ce56652ab
m-banik <mrn.banik@gmail.com>
2022-11-27 00:54:31 +0100

    Do a final code update - add comments also

--- basic_auth_project/index.js
+++ basic_auth_project/index.js
@@ -1,43 +1,43 @@
 // Requiring module
-const express = require("express");
+const express = require('express');
 var path = require('path');
 
 const app = express();
 
 function authentication(req, res, next) {
-	var authheader = req.headers.authorization;
-	console.log(req.headers);
+  var authheader = req.headers.authorization;
+  console.log(req.headers);
 
-	if (!authheader) {
-		var err = new Error('You are not authenticated!');
-		res.setHeader('WWW-Authenticate', 'Basic');
-		err.status = 401;
-		return next(err)
-	}
+  if (!authheader) {
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
 
-	var auth = new Buffer.from(authheader.split(' ')[1],
-	'base64').toString().split(':');
-	var user = auth[0];
-	var pass = auth[1];
+  var auth = new Buffer.from(authheader.split(' ')[1], 'base64')
+    .toString()
+    .split(':');
+  var user = auth[0];
+  var pass = auth[1];
 
-	if (user == '' && pass == '') {
-
-		// If Authorized user
-		next();
-	} else {
-		var err = new Error('You are not authenticated!');
-		res.setHeader('WWW-Authenticate', 'Basic');
-		err.status = 401;
-		return next(err);
-	}
-
+  // Zmieniam credentials na domyślne admin/admin
+  if (user == 'admin' && pass == 'admin') {
+    // If Authorized user
+    next();
+  } else {
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
 }
 
 // First step is the authentication of the client
-app.use(authentication)
+app.use(authentication);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Server setup
-app.listen((3000), () => {
-	console.log("Server is Running");
-})
+app.listen(3000, () => {
+  console.log('Server is Running');
+});
